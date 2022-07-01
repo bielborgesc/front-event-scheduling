f4df08971ba37c1b21b7829d0747c5ee123c34bf
Gabriel Borges <gabrielborges565@gmail.com>
2022-07-01 16:57:28 -0300

    Usando o NgStyle para deixar css dinâmico na saida do usuário

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Event Scheduling';
+
+  constructor(
+    private auth: AuthService,
+  ) { }
+
+  getVisibility(){
+    if(this.auth.isAuthenticated()){
+      return 'block';
+    }
+    return 'none'
+  }
 }
