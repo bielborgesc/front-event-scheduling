f4f8acc3888b3a819900386130e889865e96b123
Gabriel Borges <gabrielborges565@gmail.com>
2022-10-20 11:36:18 -0300

    Mudando repositorio para alocar somente o front end da apalicação

--- backend/src/helpers/regex.helper.ts
+++
@@ -1,8 +0,0 @@
-const password = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
-const date = /^(\d{4,})-(\d{2})-(\d{2})[T ](\d{2}):(\d{2})(?::(\d{2}(?:\.\d+)?))?$/;
-const status = /^.*?(PENDING|REFUSED|ACCEPT).*$/i
-export const RegExHelper = {
-  password,
-  date,
-  status
-}
--- backend/src/modules/dto/update-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import {IsNotEmpty, MinLength } from "class-validator";
-import { MessagesHelper } from "src/helpers/messages.helper";
-
-export class UpdateUserDto {  
-  @MinLength(2, {message: MessagesHelper.MIN_LENGTH_NAME_ERROR})
-  @IsNotEmpty({ message: MessagesHelper.NAME_IS_NOT_EMPTY })
-  name: string;
-}
